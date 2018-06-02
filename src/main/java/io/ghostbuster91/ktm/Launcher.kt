12bde78f5aaf859119a4bf90e42ffec3d7bad1bd
ghostbuster91 <kghost0@gmail.com>
2018-06-02 19:07:46 +0200

    Refactor

--- src/main/java/io/ghostbuster91/ktm/Launcher.kt
+++ src/main/java/io/ghostbuster91/ktm/Launcher.kt
@@ -3,6 +3,8 @@ package io.ghostbuster91.ktm
 import com.xenomachina.argparser.ArgParser
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.Response
+import okhttp3.ResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
 import okio.Okio
 import java.io.File
@@ -15,61 +17,74 @@ class ParsedArgs(parser: ArgParser) {
 }
 
 fun main(args: Array<String>) {
+    val logger: (String) -> Unit = { println(it) }
     ArgParser(args)
             .parseInto(::ParsedArgs)
             .run {
                 println("Hello, $name! $version")
-                doAll(name, version)
+                doAll(name, version, logger)
             }
 }
 
-private fun doAll(name: String, version: String) {
+private fun doAll(name: String, version: String, logger: logger) {
     val homeDir = System.getProperty("user.home")
-
     val ktmDir = File(homeDir, ".ktm")
-    val client = OkHttpClient.Builder()
-            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-            .build()
     val libDir = File(ktmDir, name.replace("/", "."))
     val versionedLibDir = File(libDir, version)
     if (!versionedLibDir.exists()) {
         versionedLibDir.mkdirs()
     }
-    println(libDir)
+    logger(libDir.toString())
+    val progressBarUpdater: (Int) -> Unit = { println(it) }
+    downloadArtifacts(name, version, logger, versionedLibDir, { a, b -> downloadFile(a, b, progressBarUpdater) })
+}
+
+private fun downloadArtifacts(name: String, version: String, logger: logger, versionedLibDir: File, fileDownloader: (String, File) -> Unit) {
     val jitPackUrl = "https://jitpack.io"
     val buildLog = URL("$jitPackUrl/$name/$version/build.log").readText()
     val files = buildLog.substringAfterLast("Files:")
-    println("files : $files")
+    logger("files : $files")
     files.split("\n")
             .filter { it.isNotBlank() }
             .drop(1)
-            .forEach {
-                println("$it")
+            .map {
+                val source = "$jitPackUrl/$name/$version/$it"
                 val destination = File(versionedLibDir, it.substringAfterLast("$version/"))
+                source to destination
+            }
+            .forEach { (source, destination) ->
                 println("Processing file $destination")
-                download("$jitPackUrl/$name/$version/$it", destination, client)
+                fileDownloader(source, destination)
             }
 }
 
-val DOWNLOAD_CHUNK_SIZE = 2048L //Same as Okio Segment.SIZE
 
-fun download(uri: String, destination: File, client: OkHttpClient) {
+private fun downloadFile(source: String, destination: File, progressBarUpdater: (Int) -> Unit) {
+    val response = download(source)
+    saveResponse(response.body()!!, destination, progressBarUpdater)
+}
+
+private fun download(uri: String): Response {
+    val client = OkHttpClient.Builder()
+            .addNetworkInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .build()
+    val request = Request.Builder().url(uri).build()
+    return client.newCall(request).execute()
+}
+
+fun saveResponse(body: ResponseBody, destination: File, progressBarUpdater: progressBarUpdater) {
+    val DOWNLOAD_CHUNK_SIZE = 2048L //Same as Okio Segment.SIZE
     try {
-        val request = Request.Builder().url(uri).build()
-
-        val response = client.newCall(request).execute()
-        val body = response.body()
-        val contentLength = body!!.contentLength()
+        val contentLength = body.contentLength()
         val source = body.source()
         val sink = Okio.buffer(Okio.sink(destination))
         var totalRead: Long = 0
-        var read: Long = 0
         val buffer = sink.buffer()
         while (!buffer.exhausted()) {
-            read = source.read(buffer, DOWNLOAD_CHUNK_SIZE)
+            val read = source.read(buffer, DOWNLOAD_CHUNK_SIZE)
             totalRead += read
             val progress = (totalRead * 100 / contentLength).toInt()
-            publishProgress(progress)
+            progressBarUpdater(progress)
         }
         sink.writeAll(source)
         sink.flush()
@@ -77,9 +92,7 @@ fun download(uri: String, destination: File, client: OkHttpClient) {
     } catch (e: IOException) {
         println(e)
     }
-
 }
 
-fun publishProgress(progress: Int) {
-    println(progress)
-}
+typealias logger = (String) -> Unit
+typealias progressBarUpdater = (Int) -> Unit
