9a5085f6bba3998192970e3c5a5021d342862fee
ghostbuster91 <kghost0@gmail.com>
2018-06-18 00:16:13 +0200

    Switch from com.xenomachina:kotlin-argparser to ajalt.clikt

--- src/main/java/io/ghostbuster91/ktm/Launcher.kt
+++ src/main/java/io/ghostbuster91/ktm/Launcher.kt
@@ -1,28 +1,40 @@
 package io.ghostbuster91.ktm
 
-import com.xenomachina.argparser.ArgParser
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.output.TermUi
+import com.github.ajalt.clikt.parameters.arguments.argument
 import io.reactivex.Observable
 import okhttp3.logging.HttpLoggingInterceptor
 import java.io.File
 import java.util.concurrent.TimeUnit
 
-class ParsedArgs(parser: ArgParser) {
-    val name by parser.positional("name of the repository")
-    val version by parser.positional("version")
+class Tool : NoRunCliktCommand()
+
+class Install : CliktCommand() {
+    private val name by argument()
+    private val version by argument()
+
+    override fun run() {
+        logger.info("Installing $name $version")
+        val homeDirProvider = { File(System.getProperty("user.home")) }
+        val jitPack = JitPackImpl(createWaitingIndicator())
+        executeInstallCommand(name, version, jitPack, homeDirProvider)
+    }
+}
+
+class Version : CliktCommand() {
+    override fun run() {
+        TermUi.echo(Build.getVersion())
+    }
 }
 
 val logger: Logger = LineWrappingLogger()
 
 fun main(args: Array<String>) {
     System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog")
-    ArgParser(args)
-            .parseInto(::ParsedArgs)
-            .run {
-                logger.info("Installing $name $version")
-                val homeDirProvider = { File(System.getProperty("user.home")) }
-                val jitPack = JitPackImpl(createWaitingIndicator())
-                executeInstallCommand(name, version, jitPack, homeDirProvider)
-            }
+    Tool().subcommands(Install(), Version()).main(args)
 }
 
 fun createWaitingIndicator() = Observable.interval(100, TimeUnit.MILLISECONDS)
