ed406a99d10804f2a78c549ea1783661652c0b49
ghostbuster91 <kghost0@gmail.com>
2018-06-18 22:53:14 +0200

    Replace ktm with fully qualified ktm installation directory

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ cd .ktm
 mkdir bin
 mkdir modules
 cd modules
-mkdir ktm
-cd ktm
+mkdir com.github.ghostbuster91:ktm
+cd com.github.ghostbuster91:ktm
 mkdir 0.0.1
 wget https://github.com/ghostbuster91/ktm/releases/download/0.0.1/ktm.tar
 tar xf ktm.tar
 rm ktm.tar
 mv ktm 0.0.1
 cd ../..
-ln -s ~/.ktm/modules/ktm/0.0.1/ktm/bin/ktm ~/.ktm/bin/ktm
+ln -s ~/.ktm/modules/com.github.ghostbuster91:ktm/0.0.1/ktm/bin/ktm ~/.ktm/bin/ktm
 echo 'Past following line into .bashrc / .zshrc etc'
 echo 'PATH=$PATH:~/.ktm/bin'
 echo 'export $PATH'
