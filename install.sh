157445477a594de2a0d0dc173d2ceda40439de04
ghostbuster91 <kghost0@gmail.com>
2018-06-19 07:11:24 +0200

    [Refactor] Make install script less verbose

--- install.sh
+++ install.sh
@@ -1,19 +1,12 @@
 #!/usr/bin/env bash
 cd ~
-mkdir .ktm
-cd .ktm
-mkdir bin
-mkdir modules
-cd modules
-mkdir com.github.ghostbuster91:ktm
-cd com.github.ghostbuster91:ktm
-mkdir 0.0.1
+mkdir -p .ktm/.modules/com.github.ghostbuster91:ktm/0.0.1
+mkdir .ktm/bin
+cd .ktm/modules/com.github.ghostbuster91:ktm
 wget https://github.com/ghostbuster91/ktm/releases/download/0.0.1/ktm.tar
 tar xf ktm.tar
 rm ktm.tar
 mv ktm 0.0.1
-cd ../..
 ln -s ~/.ktm/modules/com.github.ghostbuster91:ktm/0.0.1/ktm/bin/ktm ~/.ktm/bin/ktm
 echo 'Past following line into .bashrc / .zshrc etc'
-echo 'PATH=$PATH:~/.ktm/bin'
-echo 'export $PATH'
+echo 'export PATH=$PATH:~/.ktm/bin'
