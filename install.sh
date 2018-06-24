44e8d2d00badea765a891007f8d5055a45df8e56
ghostbuster91 <kghost0@gmail.com>
2018-06-24 15:12:15 +0200

    [InstallScript] Switch to absolute paths

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-cd ~
 mkdir -p ~/.ktm/modules/com.github.ghostbuster91:ktm/0.0.1
 mkdir ~/.ktm/bin
-echo "ktm:com.ghostbuster91" >> .ktm/aliases
-cd .ktm/modules/com.github.ghostbuster91:ktm
+echo "ktm:com.ghostbuster91" >> ~/.ktm/aliases
+cd ~/.ktm/modules/com.github.ghostbuster91:ktm
 wget https://github.com/ghostbuster91/ktm/releases/download/0.0.1/ktm.tar
 tar xf ktm.tar
 rm ktm.tar
