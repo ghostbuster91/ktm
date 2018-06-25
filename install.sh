c8913e6101533f07114fd1b334852194cc988679
ghostbuster91 <kghost0@gmail.com>
2018-06-25 12:57:54 +0200

    Fix install script by replacing : in directory path with /

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 test ! -e ~/.ktm || (echo "~/.ktm exists!"; exit 1)
-mkdir -p ~/.ktm/modules/com.github.ghostbuster91:ktm/0.0.1
+mkdir -p ~/.ktm/modules/com.github.ghostbuster91/ktm/0.0.1
 mkdir ~/.ktm/bin
 echo "ktm:com.ghostbuster91" >> ~/.ktm/aliases
-cd ~/.ktm/modules/com.github.ghostbuster91:ktm
+cd ~/.ktm/modules/com.github.ghostbuster91/ktm
 wget https://github.com/ghostbuster91/ktm/releases/download/0.0.1/ktm.tar
 tar xf ktm.tar
 rm ktm.tar
 mv ktm 0.0.1
-ln -s ~/.ktm/modules/com.github.ghostbuster91:ktm/0.0.1/ktm/bin/ktm ~/.ktm/bin/ktm
+ln -s ~/.ktm/modules/com.github.ghostbuster91/ktm/0.0.1/ktm/bin/ktm ~/.ktm/bin/ktm
 echo 'Past following line into .bashrc / .zshrc etc'
 echo 'export PATH=$PATH:~/.ktm/bin'
