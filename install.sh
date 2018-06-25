3e3dcf179512e54dffaa4654c2b2a31746528968
ghostbuster91 <kghost0@gmail.com>
2018-06-25 18:41:10 +0200

    Version 0.0.4

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
+VERSION="0.0.4"
 test ! -e ~/.ktm || (echo "~/.ktm exists!"; exit 1)
-mkdir -p ~/.ktm/modules/com.github.ghostbuster91/ktm/0.0.3
+mkdir -p ~/.ktm/modules/com.github.ghostbuster91/ktm/${VERSION}/ktm
 mkdir ~/.ktm/bin
 echo "ktm:com.ghostbuster91" >> ~/.ktm/aliases
 cd ~/.ktm/modules/com.github.ghostbuster91/ktm
-wget https://github.com/ghostbuster91/ktm/releases/download/0.0.3/ktm.tar
+wget https://github.com/ghostbuster91/ktm/releases/download/${VERSION}/ktm.tar
 tar xf ktm.tar
 rm ktm.tar
-mv ktm 0.0.3
-ln -s ~/.ktm/modules/com.github.ghostbuster91/ktm/0.0.3/ktm/bin/ktm ~/.ktm/bin/ktm
+mv ktm-${VERSION}/* ${VERSION}/ktm
+ln -s ~/.ktm/modules/com.github.ghostbuster91/ktm/${VERSION}/ktm/bin/ktm ~/.ktm/bin/ktm
 echo 'Past following line into .bashrc / .zshrc etc'
 echo 'export PATH=$PATH:~/.ktm/bin'
