6e39308dc02ab4f28789c93500922b62571ff622
ghostbuster91 <kghost0@gmail.com>
2018-07-07 10:24:31 +0200

    Add info to aliases subcommand

--- src/main/java/io/ghostbuster91/ktm/commands/Aliases.kt
+++ src/main/java/io/ghostbuster91/ktm/commands/Aliases.kt
@@ -10,7 +10,9 @@ import io.ghostbuster91.ktm.logger
 class Aliases(private val aliasRepository: AliasRepository) : CliktCommand() {
     private val artifactRegex = "([\\w.]+):([\\w.]+)".toRegex()
     private val aliasRegex = "(\\w)".toRegex()
-    private val add by option().pair().validate { (alias, artifact) -> (alias.matches(aliasRegex) && artifact.matches(artifactRegex)) || fail("Wrong input!") }
+    private val add by option(metavar = "\$alias \$artifact", help = "Use following pattern: \$alias \$artifact")
+            .pair()
+            .validate { (alias, artifact) -> (alias.matches(aliasRegex) && artifact.matches(artifactRegex)) || fail("Wrong input!") }
 
     override fun run() {
         if (add != null) {
