5a8f65559bb7d7cc6a54c654bb696f4e71fdf72c
ghostbuster91 <kghost0@gmail.com>
2018-07-08 20:14:06 +0200

    Bump version to 0.0.6

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-VERSION="0.0.5"
+VERSION="0.0.6"
 test ! -e ~/.ktm || (echo "~/.ktm exists!"; exit 1)
 mkdir -p ~/.ktm/modules/com.github.ghostbuster91/ktm/${VERSION}/ktm
 mkdir ~/.ktm/bin
