43c2e111147aadd296619bdb791bdfd9e7fa3b47
ghostbuster91 <kghost0@gmail.com>
2018-08-05 11:06:25 +0200

    Extract api to separate file

---
+++ src/main/java/io/ghostbuster91/ktm/Api.kt
@@ -0,0 +1,22 @@
+package io.ghostbuster91.ktm
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import io.ghostbuster91.ktm.components.jitpack.BuildLogApi
+import io.ghostbuster91.ktm.components.jitpack.JitPackApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
+
+private val retrofit = Retrofit.Builder()
+        .client(OkHttpClient.Builder().readTimeout(5, TimeUnit.MINUTES).build())
+        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().add(KotlinJsonAdapterFactory()).build()))
+        .baseUrl("https://jitpack.io/")
+        .build()
+val jitPackApi = retrofit.create(JitPackApi::class.java)
+val buildApi = retrofit.create(BuildLogApi::class.java)
